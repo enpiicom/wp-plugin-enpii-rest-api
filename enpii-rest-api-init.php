8328991d5eedb30a7dc3dd4a2d21c490b8322575
Loi Le, Thi Kim <loi.le@enpii.com>
2025-02-27 11:16:04 +0700

    Update wp plugin init

--- enpii-rest-api-init.php
+++ enpii-rest-api-init.php
@@ -14,40 +14,38 @@ if ( ! class_exists( Enpii_Rest_Api_WP_Plugin::class ) ) {
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Enpii_Rest_Api_Plugins_Installer.php';
 
-add_action(
-	'plugins_loaded',
-	function () {	
-		new Enpii_Rest_Api_Plugins_Installer();
+new Enpii_Rest_Api_Plugins_Installer();
 
-		$error_message = '';
-		if ( ! Enpii_Rest_Api_Helper::check_enpii_base_plugin() ) {
-			$error_message .= $error_message ? '<br />' : '';
-			$error_message .= sprintf( __( 'Plugin <strong>%s</strong> is required. 
-			Please <a href="%s">click here</a> to install and activate it first.', 'enpii-rest-api' ), 
-			'Enpii Base', 
-			Enpii_Rest_Api_Helper::get_enpii_plugins_installer_url() );
-		} else {
-			Enpii_Rest_Api_WP_Plugin::init_with_wp_app( ENPII_REST_API_PLUGIN_SLUG, __DIR__, plugin_dir_url( __FILE__ ) );
-		}
+if ( Enpii_Rest_Api_Helper::check_enpii_base_plugin() ) {
+	add_action( \Enpii_Base\App\Support\App_Const::ACTION_WP_APP_LOADED, function() {
+		Enpii_Rest_Api_WP_Plugin::init_with_wp_app(
+			ENPII_REST_API_PLUGIN_SLUG,
+			__DIR__,
+			plugin_dir_url( __FILE__ )
+		);
+	}, -111 );
+} else {
+	$error_message .= $error_message ? '<br />' : '';
+	$error_message .= sprintf( __( 'Plugin <strong>%s</strong> is required. 
+	Please <a href="%s">click here</a> to install and activate it first.', 'enpii-rest-api' ), 
+	'Enpii Base', 
+	Enpii_Rest_Api_Helper::get_enpii_plugins_installer_url() );
 
-		if ( $error_message ) {
-			add_action(
-				'admin_notices',
-				function () use ( $error_message ) {
-					$error_message = sprintf(
-						__( 'Plugin <strong>%s</strong> is not functioning.', 'enpii-rest-api' ),
-						'Enpii Rest Api Plugin'
-					) . '<br />' . $error_message;
+	if ( $error_message ) {
+		add_action(
+			'admin_notices',
+			function () use ( $error_message ) {
+				$error_message = sprintf(
+					__( 'Plugin <strong>%s</strong> is not functioning.', 'enpii-rest-api' ),
+					'Enpii Rest Api Plugin'
+				) . '<br />' . $error_message;
 
-					?>
-			<div class="notice notice-warning is-dismissible">
-					<p><?php echo $error_message; ?></p>
-			</div>
-					<?php
-				}
-			);
-		}
-	},
-	-111
-);
-
+				?>
+		<div class="notice notice-warning is-dismissible">
+				<p><?php echo $error_message; ?></p>
+		</div>
+				<?php
+			}
+		);
+	}
+}
